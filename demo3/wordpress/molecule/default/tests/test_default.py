1090df7c5c70e0e9460ad69e1c4c2cb3314c4354
Michaël BERTONI <michael@thynkio.com>
2018-09-25 23:14:11 +0200

    image docker simple, ajout conf wordpress et test

--- demo3/wordpress/molecule/default/tests/test_default.py
+++ demo3/wordpress/molecule/default/tests/test_default.py
@@ -1,5 +1,5 @@
 import os
-
+import requests
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -12,3 +12,12 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_existing_post():
+    r = requests.get('http://localhost:8000/?rest_route=/wp/v2/posts')
+    assert r.status_code == 200
+    json = r.json()
+    content = "<p>Welcome to WordPress. This is your first post." \
+        " Edit or delete it, then start writing!</p>\n"
+    assert content in json[0]['content']['rendered']
